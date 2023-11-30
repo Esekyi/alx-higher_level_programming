ed3ffcfc062b2f629fa88c635179b5e3978cc2d7
[esekyi] <[sskert10@gmail.com]>
2023-11-30 02:59:38 +0000

    fixes with pycodestyle standard

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# converting the object int into string and slicing to get last element
 last_digit_str = repr(number)[-1]
-last_digit = int(last_digit_str)  # converting string to integer
+last_digit = int(last_digit_str)
 
-if last_digit < 6 and last_digit != 0:
-    if number < 0 and last_digit != 0:
-        print("Last digit of {} is -{} and is less "
-              "than 6 and not 0".format(number, last_digit))
-    else:
-        print("Last digit of {} is {} and is "
-              "less than 6 and not 0".format(number, last_digit))
+if number < 0:
+    last_digit = -last_digit
+
+print("Last digit of {} is {} and is ".format(number, last_digit), end="")
+if last_digit == 0:
+    print("0")
 elif last_digit > 5:
-    if number > 0 and last_digit != 0:
-        print("Last digit of {} is {} and is greater "
-              "than 5".format(number, last_digit))
-    else:
-        print("Last digit of {} is -{} and is less than "
-              "6 and not 0".format(number, last_digit))
-elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("greater than 5")
+else:
+    print("less than 6 and not 0")
+
