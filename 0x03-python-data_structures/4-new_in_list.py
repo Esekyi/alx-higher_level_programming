92860c57998845574605520fca18937cacd10a26
[esekyi] <[sskert10@gmail.com]>
2023-12-07 16:52:34 -0800

    fixes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     copied_list = my_list.copy()
+
     if idx < 0:
-        return my_list
+        return (my_list)
     elif idx > len(my_list):
         return my_list
     else:
         copied_list[idx] = element
-        return copied_list
+    return copied_list
