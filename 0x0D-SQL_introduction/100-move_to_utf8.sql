3b59c262870d5db21b39e20d570f7bcfa89f588f
[esekyi] <[sskert10@gmail.com]>
2024-05-22 19:39:13 +0000

    17. Go to UTF8 - fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,10 @@
--- a script that converts hbtn_0c_0 database and first_table (including the field name) to UTF8
+-- Script to convert hbtn_0c_0 database and its components to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+-- Convert the database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert specific field in the table
+ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
