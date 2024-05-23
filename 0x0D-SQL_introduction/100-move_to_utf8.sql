dbfc4f96fa70f32fb8bebbf1b478bc11847dc647
[esekyi] <[sskert10@gmail.com]>
2024-05-23 21:52:46 +0000

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,4 @@
--- Script to convert hbtn_0c_0 database and its components to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
-
+-- Script to convert hbtn_0c_0 database and its components to UTF8
 -- Convert the database
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert specific field in the table
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
