e01a914190080edf599f8c9b4494fb94e02e3f0a
[esekyi] <[sskert10@gmail.com]>
2024-05-24 03:38:20 +0000

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,4 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
-/*
-cities description:
-id INT unique, auto generated, can’t be null and is a primary key
-state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
-name VARCHAR(256) can’t be null
-*/
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
@@ -14,5 +8,4 @@ CREATE TABLE IF NOT EXISTS cities (
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
-) type=InnoDB;
-
+);
