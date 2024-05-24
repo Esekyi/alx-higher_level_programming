2431cbd40b62cf50dd7c9083509438f1107f6f96
[esekyi] <[sskert10@gmail.com]>
2024-05-24 04:40:18 +0000

    10. fixes

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,6 @@
--- a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_genres.name, tv_show_genres.genre_id 
-FROM tv_genres 
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
-WHERE tv_shows.title = "Dexter" 
-ORDER BY tv_genres.name ASC;
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+ORDER BY tv_shows.title ASC tv_show_genres.genre_id ASC;
 
