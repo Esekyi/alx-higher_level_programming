7f243d7fc90a18afe8437e6a26cb1c72221f02e7
[esekyi] <[sskert10@gmail.com]>
2024-06-10 01:48:25 +0000

    print with string formart

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 results = "".join(chr(i) if i % 2 == 0 else chr(i).upper()
-      for i in range(ord('z'), ord('a') - 1, -1))
+                  for i in range(ord('z'), ord('a') - 1, -1))
 
-print(f"{results}")
+print("{:s}".format(results), end="")
