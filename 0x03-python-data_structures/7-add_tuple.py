aa64cafb2e4741121cfc499ed5cfa9af3b199266
[esekyi] <[sskert10@gmail.com]>
2024-06-13 15:08:14 +0000

    7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    pass
+    a1, a2 = 0, 0
+    b1, b2 = 0, 0
+
+    if len(tuple_a) > 0:
+        a1 = tuple_a[0]
+
+    if len(tuple_a) > 1:
+        a2 = tuple_a[1]
+
+    if len(tuple_b) > 0:
+        b1 = tuple_b[0]
+
+    if len(tuple_b) > 1:
+        b2 = tuple_b[1]
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+
+    return (sum1, sum2)
