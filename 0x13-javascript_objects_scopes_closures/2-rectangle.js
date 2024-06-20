851a666932297469b2d01889e7e57124808ff279
[esekyi] <[sskert10@gmail.com]>
2024-06-20 04:49:40 +0000

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
-	  if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
-      return `Rectangle {}`;
+    if (typeof w !== 'number' || typeof h !== 'number' || w <= 0 || h <= 0) {
+      return 'Rectangle {}';
     }
     this.width = w;
     this.height = h;
