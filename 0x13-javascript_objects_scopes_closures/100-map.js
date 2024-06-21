fb312c968eebdbecab267951b777ae0d1aabbd9d
[esekyi] <[sskert10@gmail.com]>
2024-06-21 02:00:10 +0000

    keeps failing

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,5 @@
 
 const list = require('./100-data.js').list;
 
-const newList = list.map((value, index) => value * index);
-
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
