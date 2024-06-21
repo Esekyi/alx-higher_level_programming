b7cdff7c0f3450a769f7a29b47ab3ec834a859fc
[esekyi] <[sskert10@gmail.com]>
2024-06-21 02:46:03 +0000

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const args = process.argv;
+const phrase = 'C is fun';
+
+if (args[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < args[2]; i++) {
+    console.log(phrase);
+  }
+}
