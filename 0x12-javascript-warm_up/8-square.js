13adf7a31c45b2040738e03514eb62d58f8b0f30
[esekyi] <[sskert10@gmail.com]>
2024-06-21 03:00:02 +0000

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const args = process.argv;
+
+if (args[2] === undefined || (isNaN(args[2])) || args[2] < 0) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < args[2]; i++) {
+    console.log('X'.repeat(args[2]));
+  }
+}
