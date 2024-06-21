a2bc1f9308dfe02dd6241b5275567b8c95c07a01
[esekyi] <[sskert10@gmail.com]>
2024-06-21 03:27:26 +0000

    10. Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const args = process.argv;
+const integer = parseInt(args[2]);
+
+function factorial (num) {
+  if (isNaN(num) || num <= 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(integer));
