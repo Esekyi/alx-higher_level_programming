a82d9bad9229a2b087a860ba00420e3d0b566f1a
[esekyi] <[sskert10@gmail.com]>
2024-06-24 04:42:49 +0000

    7-All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,6 +11,9 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    Main function
+    """
     user = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
