4d0c13c1afdd9e12b610e58f1b7c0a8175e413d8
[esekyi] <[sskert10@gmail.com]>
2024-06-25 05:33:46 +0000

    10. Get a state

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter_by(State.name.contains('a'))
+    state = session.query(State).filter(State.name.contains('a'))
     if state is not None:
         for instance in state:
             print("{0}: {1}".format(instance.id, instance.name))
