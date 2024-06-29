7db45d1ea751cf0415ec122b80fe8dc02b2a7cf9
[esekyi] <[sskert10@gmail.com]>
2024-06-29 23:48:08 +0000

    13. delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-Python Script that changes the name of a State
-object from the database hbtn_0e_6_usa
-it takes 4 arguments: mysql username, mysql password,
-database name and the new name of the state
+Python Script that deletes all State objects with a name containing
+the letter `a` from the database `hbtn_0e_6_usa`
 """
 
 import sys
@@ -12,10 +10,10 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     """
     Main function
-    Access to the database and update the state
-    with a new name
+    Access to the database and delete the states
     """
     user = sys.argv[1]
     password = sys.argv[2]
@@ -25,10 +23,10 @@ if __name__ == "__main__":
                            .format(user, password, db_name),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    update_state = session.query(State).filter(State.id == 2).first()
-    update_state.name = "New Mexico"
+    delete_states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in delete_states:
+        session.delete(state)
     session.commit()
     session.close()
