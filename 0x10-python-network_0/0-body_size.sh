7a9c888c0495c3f416ab7c40da9d0b6649d03e49
[esekyi] <[sskert10@gmail.com]>
2024-07-05 03:48:13 +0000

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/bin/bash
 # bash script that takes in a URL, sends a request to that URL, and displays the size of the body in the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f 2
