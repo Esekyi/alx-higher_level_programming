19db05fa1be413c63c1923790ffa1bd238cb620c
[esekyi] <[sskert10@gmail.com]>
2024-07-05 04:07:07 +0000

    2. cURL Method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# bash script that takes in a URL, sends a GET request to the URL, and displays the body
 curl -sL "$1"
