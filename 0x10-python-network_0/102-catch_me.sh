fe42e3c199c3a335a9141f7a375186fa1db9fb00
[esekyi] <[sskert10@gmail.com]>
2024-07-05 05:27:01 +0000

    9. Catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # bash script that makes a request to a sandbox url, causes the server to respond with a message, "	You got me!"
-curl -sL -X PUT "0.0.0.0:5000/catch_me" -d "user_id=98" -H "Origin: School"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: School"
