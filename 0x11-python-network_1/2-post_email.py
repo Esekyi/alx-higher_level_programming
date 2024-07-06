76b58f2207963c9fbbb51f7fbd492d6240fd30c9
[esekyi] <[sskert10@gmail.com]>
2024-07-06 05:10:51 +0000

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Python script, takes in a URL and email, sends
+a POST request and displays the body of the response in utf-8
+"""
+
+
+if __name__ == "__main__":
+    """
+    Main Function
+    Python script, takes in a URL and email, sends a POST request
+    and displays the body of the response in utf-8
+    """
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Your email is: {}".format(html.decode('utf-8')))
