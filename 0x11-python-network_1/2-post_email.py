d93247e3f2aaeb02f4c5352d1c7037055fe3bd6f
[esekyi] <[sskert10@gmail.com]>
2024-07-06 05:36:31 +0000

    2. POST - fixes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         html = response.read()
-        print(html.decode('utf-8'))
+        print("Your email is: {}".format(values['email']))
