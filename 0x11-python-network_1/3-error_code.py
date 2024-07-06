b602b55c7193e87f72130430a36dab4fa9726d45
[esekyi] <[sskert10@gmail.com]>
2024-07-06 05:22:31 +0000

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,21 @@
-"""Python script, takes in a URL and email, sends
-a POST request and displays the body of the response in utf-8
+"""Python script, takes in a URL, sends a request
+and displays the body of the response in utf-8
 """
 
 
 if __name__ == "__main__":
     """
     Main Function
-    Python script, takes in a URL and email, sends a POST request
+    Python script, takes in a URL, sends a request
     and displays the body of the response in utf-8
     """
     import urllib.request
-    import urllib.parse
     import sys
 
     url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
