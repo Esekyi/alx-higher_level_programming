472bf3c509843d4ecc73d19bfc8e65a092b8aa3f
[esekyi] <[sskert10@gmail.com]>
2024-07-31 05:49:51 +0000

    fixes 4.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,14 @@
 
 const request = require('request');
 const url = process.argv[2];
-const wedgeAntilles = 18;
-const characterUrl = 'https://swapi-api.alx-tools.com/api/people/1/';
+// const wedgeAntilles = 18;
+// const characterUrl = 'https://swapi-api.alx-tools.com/api/people/1/';
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-
+    const data = JSON.parse(body);
+    console.log(data.results);
   }
 });
