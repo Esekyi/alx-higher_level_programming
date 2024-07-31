aabcea93ce7654d62a6a43a974d596b7aea0f0d5
[esekyi] <[sskert10@gmail.com]>
2024-07-31 05:24:58 +0000

    fixes & 3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const movieID = process.argv[2];
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/';
+const url = endpoint + movieID;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
