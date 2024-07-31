d878f700502e74167bbd969a2ad2b22ffa057453
[esekyi] <[sskert10@gmail.com]>
2024-07-31 04:57:39 +0000

    2. Status code - fixes

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
 const url = process.argv[2];
@@ -7,6 +7,6 @@ request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
