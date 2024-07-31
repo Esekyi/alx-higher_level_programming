80c0f25c52f602fcaf6b68547aedadca8c76e661
[esekyi] <[sskert10@gmail.com]>
2024-07-31 04:32:07 +0000

    1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const addString = process.argv[3]
+
+fs.writeFile(filePath, addString, 'utf-8', (err) => {
+  if (err) {
+	console.error(err)
+  }
+});
